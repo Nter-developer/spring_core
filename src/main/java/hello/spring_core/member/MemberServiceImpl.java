624ce9b46d25880fb372ab2bff455eec817ebdbc
Sanghyun Yi <njsharry32@gmail.com>
2022-08-08 15:51:35 +0900

    create spring container

--- src/main/java/hello/spring_core/member/MemberServiceImpl.java
+++ src/main/java/hello/spring_core/member/MemberServiceImpl.java
@@ -1,8 +1,13 @@
 package hello.spring_core.member;
 
+import hello.spring_core.discount.DiscountPolicy;
+
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository  = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
